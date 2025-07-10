35da3934e542939de8cd2d3ecb7e026f7a49ec88
Eduardo Sampaio <duardosampaiio456@gmail.com>
2025-07-10 17:33:41 -0300

    test: conectando mongoDb e criando Schema

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const User = require("../models/User")
+
+router.post('/users', async (req, res) => {
+    try {
+        const { nome, sobrenome, email } = req.body
+        const novoUsuario = new User({ nome, sobrenome, email })
+        const usuarioSalvo = await novoUsuario.save()
+        return res.status(201).json(usuarioSalvo)
+
+    } catch (error) {
+        res.status(500).json({ error: error.mesage })
+    }
+
+})
+
+
+
+module.exports = router
---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI;
+const app = express()
+app.use(express.json())
+const router = require('../routes/users')
+app.use(router)   
+
+// conectando ao DB
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Conectado ao MongoDB!'))
+  .catch((err) => console.error('Erro ao conectar:', err));
+
+app.get('/', (req, res)=>{
+    res.send('bem vindo ao meu server!')
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`servidor rodando na porta ${PORT}`);
+    
+})
