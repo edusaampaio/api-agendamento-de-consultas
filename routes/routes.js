bf0c266055dceef5d9c7e8237c7e3c0f5e1bbd62
Eduardo Sampaio <eduardosampaiio456@gmail.com>
2025-07-12 13:29:57 -0300

    feat: corrigindo erros do import

--- routes/routes.js
+++ routes/routes.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const homePageController = require('../controllers/users/homePage');
 const cadastroUsuario = require('../controllers/users/cadastroUsuario');    
 router.get('/', homePageController.homePage);
-router.post('/users', cadastroUsuario);
+router.post('/users', cadastroUsuario.cadastroUsuario);
+
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const User = require("../models/users")
+const User = require("../models/Users/users")
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI;
 const app = express()
+const router = require('../routes/routes')
 app.use(express.json())
-const router = require('../routes/users')
 app.use(router)   
-
 // conectando ao DB
 mongoose.connect(MONGO_URI)
   .then(() => console.log('Conectado ao MongoDB!'))
